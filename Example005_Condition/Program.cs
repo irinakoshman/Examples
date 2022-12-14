c184161d59662cd2756109484a6c364e133d9ce7
STGorbunovDA <bous07@mail.ru>
2022-12-15 01:18:37 +0800

    Add Example005_Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,14 @@
+﻿using Internal;
+using System;
+Console.Write("Введите имя пользователя: ");
+string userName = Console.Redline();
+
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
